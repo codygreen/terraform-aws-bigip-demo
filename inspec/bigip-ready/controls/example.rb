e7335fed2877c02ff1ca5209b621b549c7220d25
Mark Menger <m.menger@f5.com>
2019-10-21 15:01:08 -0700

    include AS3 operability test

--- inspec/bigip-ready/controls/example.rb
+++ inspec/bigip-ready/controls/example.rb
@@ -10,6 +10,7 @@ params = JSON.parse(content)
 
 BIGIP_DNS  = params['bigip_mgmt_public_ips']['value']
 BIGIP_PORT = params['bigip_mgmt_port']['value']
+BIGIP_PASSWORD  = params['bigip_password']['value']
 
 control "Connectivity" do
   impact 1.0
@@ -19,7 +20,16 @@ control "Connectivity" do
     describe host(bigip_host, port: BIGIP_PORT, protocol: 'tcp') do
         it { should be_reachable }
     end
-    # TODO: include testing of authentication against API
+    testurl = 'https://' + bigip_host + ':' + BIGIP_PORT + '/mgmt/shared/appsvcs/info'
+    describe http(testurl,
+              auth: {user: 'admin', pass: BIGIP_PASSWORD},
+              params: {format: 'html'},
+              method: 'GET',
+              ssl_verify: false) do
+          its('status') { should cmp 200 }
+          its('body') { should match 'version' }
+          its('headers.Content-Type') { should match 'application/json' }
+    end
   end
 end 
 
