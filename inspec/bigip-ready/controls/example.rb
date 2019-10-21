1bef9e8dedfbdba8f765ffed4259deccd3f93879
Mark Menger <m.menger@f5.com>
2019-10-21 14:52:29 -0700

    test for AS3 availability

--- inspec/bigip-ready/controls/example.rb
+++ inspec/bigip-ready/controls/example.rb
@@ -8,20 +8,29 @@ title "Verify BIG-IP availability"
 content = inspec.profile.file("terraform.json")
 params = JSON.parse(content)
 
-BIGIP_DNS  = params['bigip_mgmt_public_ips']['value']
-BIGIP_PORT = params['bigip_mgmt_port']['value']
+BIGIP_DNS       = params['bigip_mgmt_public_ips']['value']
+BIGIP_PORT      = params['bigip_mgmt_port']['value']
+BIGIP_PASSWORD  = params['bigip_password']['value']
 
-control "Connectivity" do
+control "AS3 Connectivity" do
   impact 1.0
-  title "BIGIP is reachable"
+  title "BIGIP is ready and reachable"
 
   BIGIP_DNS.each do |bigip_host|
     describe host(bigip_host, port: BIGIP_PORT, protocol: 'tcp') do
         it { should be_reachable }
     end
-    # TODO: include testing of authentication against API
+    testurl = 'https://' + bigip_host + ':' + BIGIP_PORT + '/mgmt/shared/appsvcs/info'
+    describe http(testurl,
+              auth: {user: 'admin', pass: BIGIP_PASSWORD},
+              params: {format: 'html'},
+              method: 'GET',
+              ssl_verify: false) do
+          its('status') { should cmp 200 }
+          its('body') { should match 'version' }
+          its('headers.Content-Type') { should match 'application/json' }
+    end
   end
 end 
-
 
 
