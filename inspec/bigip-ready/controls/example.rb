7ef3ee85fbf8f2c931d6ebe3a32a9699cca0f9f3
Mark Menger <m.menger@f5.com>
2019-10-21 19:37:22 -0700

    fix a testing comment

--- inspec/bigip-ready/controls/example.rb
+++ inspec/bigip-ready/controls/example.rb
@@ -4,7 +4,7 @@ title "Verify BIG-IP availability"
 
 
 # load data from Terraform output
-# created by terraform output --json > bigip/files/terraform.json
+# created by terraform output --json > inspec/bigip-ready/files/terraform.json
 content = inspec.profile.file("terraform.json")
 params = JSON.parse(content)
 
