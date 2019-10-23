84f35cc148e6acabf80bb8cb5f96b7a7eeffc5be
Mark Menger <m.menger@f5.com>
2019-10-23 13:14:11 -0700

    simplify running inspec tests and extend tests

--- inspec/bigip-ready/controls/example.rb
+++ inspec/bigip-ready/controls/example.rb
@@ -17,17 +17,18 @@ control "Connectivity" do
   title "BIGIP is reachable"
 
   BIGIP_DNS.each do |bigip_host|
+    # can we reach the management port on the BIG-IP?
     describe host(bigip_host, port: BIGIP_PORT, protocol: 'tcp') do
         it { should be_reachable }
     end
-    testurl = 'https://' + bigip_host + ':' + BIGIP_PORT + '/mgmt/shared/appsvcs/info'
-    describe http(testurl,
+    # is the application services end point available?
+    describe http("https://#{bigip_host}:#{BIGIP_PORT}/mgmt/shared/appsvcs/info",
               auth: {user: 'admin', pass: BIGIP_PASSWORD},
               params: {format: 'html'},
               method: 'GET',
               ssl_verify: false) do
           its('status') { should cmp 200 }
-          its('body') { should match 'version' }
+          its('body') { should match 'version' } # this should be replaced with a more nuanced set of tests
           its('headers.Content-Type') { should match 'application/json' }
     end
   end
