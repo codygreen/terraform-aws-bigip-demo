c9082f6654e86e44e08c0bc06f96e473fbcb90f5
Mark Menger <m.menger@f5.com>
2019-10-28 15:48:15 -0700

    alternate networking

--- network.tf
+++ network.tf
@@ -1,5 +1,6 @@
 #
 # Create the VPC 
+# using directions from https://clouddocs.f5.com/cloud/public/v1/aws/AWS_multiNIC.html
 #
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -11,25 +12,27 @@ module "vpc" {
 
   azs = var.azs
 
+  # vpc public subnet used for external interface
   public_subnets = [for num in range(length(var.azs)) :
-      cidrsubnet(var.cidr, 8, num)
+      cidrsubnet(var.cidr, 8, num + var.external_subnet_offset)
   ]
   
+  # vpc private subnet used for internal 
+  private_subnets = [
+    for num in range(length(var.azs)) :
+    cidrsubnet(var.cidr, 8, num + var.internal_subnet_offset)
+  ]
+
+  enable_nat_gateway = true
+
   # using the database subnet method since it allows a public route
   database_subnets = [
     for num in range(length(var.azs)) :
-    cidrsubnet(var.cidr, 8, num + 10)
+    cidrsubnet(var.cidr, 8, num + var.management_subnet_offset)
   ]
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
   create_database_internet_gateway_route = true
-
-  private_subnets = [
-    for num in range(length(var.azs)) :
-    cidrsubnet(var.cidr, 8, num + 20)
-  ]
-
-  enable_nat_gateway = true
 
   tags = {
     Name        = format("%s-vpc-%s", var.prefix, random_id.id.hex)
@@ -37,5 +40,6 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
 
 
