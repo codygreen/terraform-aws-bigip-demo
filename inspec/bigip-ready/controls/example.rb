faafbce49e3b6dfc08e50eff5edfb379f82926fe
Mark Menger <m.menger@f5.com>
2019-10-28 18:46:22 -0700

    update required DO and TS versions for testing

--- inspec/bigip-ready/controls/example.rb
+++ inspec/bigip-ready/controls/example.rb
@@ -49,7 +49,7 @@ control "Declarative On-boarding Available" do
               params: {format: 'html'},
               method: 'GET',
               ssl_verify: false).body) do
-          its([0,'version']) { should eq '1.7.0' }
+          its([0,'version']) { should eq '1.8.0' }
           its([0,'release']) { should eq '3' } # this should be replaced with a test using the json resource
     end
   end
@@ -99,8 +99,8 @@ control "Telemetry Streaming Available" do
               params: {format: 'html'},
               method: 'GET',
               ssl_verify: false).body) do
-          its('version') { should eq '1.0.0' }
-          its('release') { should eq '2' } # this should be replaced with a test using the json resource
+          its('version') { should eq '1.6.0' }
+          its('release') { should eq '1' } # this should be replaced with a test using the json resource
     end
   end
 end 
