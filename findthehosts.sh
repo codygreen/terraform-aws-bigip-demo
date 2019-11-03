038b685f5233ef43f9b85ab8fe19034a4eb810ad
Mark Menger <m.menger@f5.com>
2019-11-03 10:30:59 -0800

    update findthehosts helper with juiceshop and grafana public ips

--- findthehosts.sh
+++ findthehosts.sh
@@ -5,12 +5,25 @@ export BIGIPPASSWORD=`terraform output --json | jq -r '.bigip_password.value'`
 export EC2KEYNAME=`terraform output --json | jq -r '.ec2_key_name.value'`
 export JUMPHOSTIP0=`terraform output --json | jq -r '.jumphost_ip.value[0]'`
 export JUMPHOSTIP1=`terraform output --json | jq -r '.jumphost_ip.value[1]'`
-echo connect at https://$BIGIPHOST0:$BIGIPMGMTPORT with $BIGIPPASSWORD
+export JUICESHOP0=`terraform output --json | jq -r '.juiceshop_ip.value[0]'`
+export JUICESHOP1=`terraform output --json | jq -r '.juiceshop_ip.value[1]'`
+export GRAFANA0=`terraform output --json | jq -r '.grafana_ip.value[0]'`
+export GRAFANA1=`terraform output --json | jq -r '.grafana_ip.value[1]'`
+echo '** AVAILABILITY ZONE 1 **'
+echo connect to BIG=-IP at https://$BIGIPHOST0:$BIGIPMGMTPORT with $BIGIPPASSWORD
 echo connect to jumphost at with
 echo scp -i $EC2KEYNAME.pem $EC2KEYNAME.pem ubuntu@$JUMPHOSTIP0:~/$EC2KEYNAME.pem
 echo ssh -i $EC2KEYNAME.pem ubuntu@$JUMPHOSTIP0
+echo when the ansible run is complete Juiceshop and Grafana should be available at
+echo Juice Shop http://$JUICESHOP0
+echo Grafana http://$GRAFANA0
+echo 
 echo
-echo connect at https://$BIGIPHOST1:$BIGIPMGMTPORT with $BIGIPPASSWORD
+echo '** AVAILABILITY ZONE 2 **'
+echo connect to BIG-IP at https://$BIGIPHOST1:$BIGIPMGMTPORT with $BIGIPPASSWORD
 echo connect to jumphost at with
 echo scp -i $EC2KEYNAME.pem $EC2KEYNAME.pem ubuntu@$JUMPHOSTIP1:~/$EC2KEYNAME.pem
 echo ssh -i $EC2KEYNAME.pem ubuntu@$JUMPHOSTIP1
+echo when the ansible run is complete Juiceshop and Grafana should be available at
+echo Juice Shop http://$JUICESHOP1
+echo Grafana http://$GRAFANA1
